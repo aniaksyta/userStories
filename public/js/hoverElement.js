e8054de705a4661c5a49d5d5e0786cd0a63f9c6b
Anna <ksytaa@gmail.com>
2015-05-15 15:53:47 +0200

    poprawka js + transition

--- public/js/hoverElement.js
+++ public/js/hoverElement.js
@@ -1,12 +1,10 @@
 $(document).ready(function(){
-	$('.container .text-center > div').bind('mouseover', function () {
-	    $(this).addClass('hoversDiv');
-	    $(this).find('.btn-group > .btn:first-child').removeClass( "btn btn-primary btn-sm disabled" ).addClass( "btn btn-primary btn-sm" );
-	    $('.container .text-center > div').not('.hoversDiv').animate({
-	    });
+	$('.container .price-item').bind('mouseover', function () {
+	    $(this).addClass('active');
+	    $(this).find('.btn').removeClass('disabled');
 	});
-	$('.container .text-center > div').bind('mouseout', function () {
-		$(this).find('.btn-group > .btn:first-child').removeClass( "btn btn-primary btn-sm" ).addClass( "btn btn-primary btn-sm disabled" );
-	    $('.container .text-center > div').removeClass('hoversDiv').fadeTo('normal', 1);
+	$('.container .price-item').bind('mouseout', function () {
+		$(this).removeClass('active');
+		$(this).find('.btn').addClass('disabled');
 	});
     });
